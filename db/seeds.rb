7c0c49501c0ed9932cd27196e50d50c239eeaeed
djerambetefrederic <freddydjerra@yahoo.com>
2023-11-16 11:29:39 +0100

    rails yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "01252466346", category: "chinese" }
+bstreet = {name: "bstreet", address: "Evry", phone_number: "01252466346", category: "chinese" }
+bk = {name: "bk", address: "spot", phone_number: "01252466346", category: "chinese" }
+pittaya = {name: "pittaya", address: "Spot2", phone_number: "01252466346", category: "chinese" }
+pizza = {name: "Pizza", address: "Evry 3", phone_number: "01252466346", category: "chinese" }
+
+[dishoom, bstreet, bk, pittaya, pizza].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
